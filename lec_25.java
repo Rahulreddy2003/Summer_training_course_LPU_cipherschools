909cda4a8d1b2a847b123b3dad55a72437ebeaf1
Rahulreddy2003 <122749010+Rahulreddy2003@users.noreply.github.com>
2023-07-19 18:13:32 +0530

    lec_25.java

---
+++ lec_25.java
@@ -0,0 +1,34 @@
+public class BinarySearch {
+    public static int binarySearch(int[] array, int target) {
+        int left = 0;
+        int right = array.length - 1;
+        
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            
+            if (array[mid] == target) {
+                return mid;
+            }
+            
+            if (array[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        
+        return -1; // Target element not found
+    }
+    
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int target = 6;
+        int result = binarySearch(array, target);
+        
+        if (result == -1) {
+            System.out.println("Element not found");
+        } else {
+            System.out.println("Element found at index: " + result);
+        }
+    }
+}
